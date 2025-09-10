a12a5b803b109020f926f4364ac527ff281b06c6
refensly <103318606+refensly@users.noreply.github.com>
2025-09-10 15:52:52 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -216,7 +216,7 @@ const Index = () => {
     title: "Wood Interior",
     subtitle: "Interior Shelving in Solid Oak",
     caption: "Solid oak interior elements: shelving, wall panels, and ceiling cladding",
-    image: '/lovable-uploads/4a4f91cb-5dd1-44e9-b7cd-372112468fc1.png',
+    image: '/lovable-uploads/bbb9e0f0-047a-4dd3-9269-37913f6d3ae3.png',
     images: ['/lovable-uploads/bbb9e0f0-047a-4dd3-9269-37913f6d3ae3.png', '/lovable-uploads/2bde928f-03af-4cbf-9149-c9a73b4166ba.png', '/lovable-uploads/4a4f91cb-5dd1-44e9-b7cd-372112468fc1.png']
   }];
   const processSteps = [{
